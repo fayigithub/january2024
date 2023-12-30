fd464c7156572f47c51696889636742d3e927124
unknown <faysalmuhammed22@gmail.com>
2023-12-30 01:47:34 +0100

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "default" {
-  allocated_storage           = 20
+  allocated_storage           = 50
   db_name                     = "mydb"
   engine                      = "mysql"
   engine_version              = "5.7"
